c71dc254170d71cb17fde087a2188063df87b138
zhaopanpan <zhaopanpan>
2022-06-08 15:36:38 +0800

    优化

--- docs/.vuepress/theme/components/page/index.vue
+++ docs/.vuepress/theme/components/page/index.vue
@@ -20,18 +20,13 @@ import { resolveSidebarItems } from '@theme/utils/index'
 export default {
   components: { PageNav,SidebarRight },
   props: ['sidebarItems'],
-  watch: {
-    '$page' (val) {
-     
-    }
-  },
   mounted(){
-    console.log(this.$page)
-    console.log(this.$page.regularPath)
-    console.log(this.$site)
-    console.log(this.$localePath)
-    const a = resolveSidebarItems(this.$page,this.$page.regularPath,this.$site,this.$localePath)
-    console.log(a)
+    // console.log(this.$page)
+    // console.log(this.$page.regularPath)
+    // console.log(this.$site)
+    // console.log(this.$localePath)
+    // const a = resolveSidebarItems(this.$page,this.$page.regularPath,this.$site,this.$localePath)
+    // console.log(a)
   },
   methods:{
     header(){
