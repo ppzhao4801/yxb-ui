cdc593562a702b552fd180e8a99eeb4868c36e6e
zhaopanpan <zhaopanpan>
2022-06-09 13:14:45 +0800

    优化

--- docs/.vuepress/theme/utils/active-bar.js
+++ docs/.vuepress/theme/utils/active-bar.js
@@ -105,7 +105,7 @@ function getPageOffset() {
 function getAnchorTop(anchor) {
   const pageOffset = getPageOffset()
   try {
-    return anchor.parentElement && anchor.parentElement.offsetTop - pageOffset + 70
+    return anchor.parentElement && anchor.parentElement.offsetTop - pageOffset + 100
   } catch {
     return 0
   }
