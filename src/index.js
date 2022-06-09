b6bfbd6109944c12e739caf315da33d4309bd950
zhaopanpan <zhaopanpan>
2022-06-09 17:34:12 +0800

    优化

--- src/index.js
+++ src/index.js
@@ -189,6 +189,15 @@ const install = function(Vue, opts = {}) {
     size: opts.size || '',
     zIndex: opts.zIndex || 2000
   };
+
+  Vue.prototype.$loading = Loading.service;
+  Vue.prototype.$msgbox = MessageBox;
+  Vue.prototype.$alert = MessageBox.alert;
+  Vue.prototype.$confirm = MessageBox.confirm;
+  Vue.prototype.$prompt = MessageBox.prompt;
+  Vue.prototype.$notify = Notification;
+  Vue.prototype.$message = Message;
+  
 };
 
 /* istanbul ignore if */
