1738c97fdcc29f36f190475aab71aa31875c56ed
zhaopanpan <zhaopanpan>
2022-06-09 17:47:28 +0800

    优化

--- test/components/initData.js
+++ test/components/initData.js
@@ -3,7 +3,8 @@ var path = require('path');
 var Components = require('./data.json');
 Components = Object.keys(Components);
 
-const exclusives=["button","message","message-box","notification"]
+const exclusives=["button"]
+const notNames = ["message","message-box","notification"]
 Components.forEach(function(key) {
     if(exclusives.indexOf(key)>-1){
       return false
@@ -21,7 +22,13 @@ Components.forEach(function(key) {
     }else{
       upperCaseKey=key.slice(0,1).toUpperCase() + key.slice(1)
     }
-    const content = `import ${upperCaseKey} from "element-ui/lib/${key}";\n${upperCaseKey}.name="Yxb${upperCaseKey}"\nexport default ${upperCaseKey};`;
+    let content;
+    if(notNames.indexOf(key)>-1){
+      content = `import ${upperCaseKey} from "element-ui/lib/${key}";\nexport default ${upperCaseKey};`;
+    }else{
+      content = `import ${upperCaseKey} from "element-ui/lib/${key}";\n${upperCaseKey}.name="Yxb${upperCaseKey}"\nexport default ${upperCaseKey};`;
+    }
+    
     var paths=path.resolve(filePath,'index.js')
     fs.writeFileSync(path.resolve(filePath,'index.js'), content, 'utf8');
 });
